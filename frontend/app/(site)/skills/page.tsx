c4f97938a80a21cdd3da48fb9f64d357b9cc5d19
abiel13 <dbestabi28@gmail.com>
2024-02-24 15:36:38 +0100

    skills

--- frontend/app/(site)/skills/page.tsx
+++ frontend/app/(site)/skills/page.tsx
@@ -9,8 +9,9 @@ const SkillsPage = () => {
     <div className="w-full flex flex-col items-center mt-[4rem]">
       <div className="flex w-[70%] px-4 items-center justify-between ">
         <div>add skills</div>
-NPM         <div className="px-3 py-2 rounded-md bg-[#6FFFE9] min-w-[300px]">
+        <div className="px-3 py-2 rounded-md bg-[#6FFFE9] min-w-[300px]">
           {" "}
+          <CldUpload />
           forgot something{" "}
         </div>
       </div>
