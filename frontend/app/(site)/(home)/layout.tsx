252cf9827e2d452004e85c0ea3a6327d877f0a32
abiel13 <dbestabi28@gmail.com>
2024-02-29 21:46:22 +0100

    version 1 stable ui release

---
+++ frontend/app/(site)/(home)/layout.tsx
@@ -0,0 +1,39 @@
+"use client";
+import React from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+export default function HomeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+  return (
+    <div className="px-3 py-3 flex flex-col gap-4">
+      <div className="flex items-center rounded-md justify-between gap-2 py-1 bg-sidebar-bg  px-1 min-w-[190px] max-w-[200px] ">
+        <Link
+          href={"/"}
+          className={`text-center px-2 py-2 rounded-md ${
+            pathname == "/" ? "bg-[#1E2231] text-white" : "text-main-light"
+          }`}
+        >
+          For You
+        </Link>
+        <Link
+          href={"/following"}
+          className={`text-center px-2  flex-1 py-2 rounded-md  ${
+            pathname == "/following"
+              ? "bg-[#1E2231] text-white"
+              : "text-main-light"
+          }`}
+        >
+          Following
+        </Link>
+      </div>
+
+      <div className="bg-sidebar-bg py-3 px-2 text-main-light">write something for the community</div>
+      {children}
+    </div>
+  );
+}
--- frontend/app/(site)/(home)/page.tsx
+++ frontend/app/(site)/(home)/page.tsx
@@ -5,7 +5,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className=" flex flex-col  px-3 py-2  h-full gap-6">
-   
+
+    
     </div>
   );
 }
