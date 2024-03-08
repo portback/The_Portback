33ae67a4c82dddad2e3542b2b6199f66f1c1f773
abiel13 <dbestabi28@gmail.com>
2024-03-08 18:24:27 +0100

    cleanup

--- frontend/lib/actions/post.ts
+++ frontend/lib/actions/post.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 
 export const getAllPost = async () => {
   try {
-    const response = await axios.get("http://localhost:5000/api/v1/post");
+    const response = await axios.get("https://portfolio-hub-server.onrender.com/api/v1/post");
     return response.data;
   } catch (err:any) {
     console.log(err.message);
